4974e769fa91d8f065e5597b5967efc9245df643
Santosh Dapkar <santoshdapkar7447@gmail.com>
2026-02-14 02:08:10 +0530

    Removed db.js from repo

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect(
-      "mongodb+srv://santosh:radheradhe@cluster0.5kbjysf.mongodb.net/food-del"
-    ).then(() =>
-      console.log("DB connected"));
-  
-}
